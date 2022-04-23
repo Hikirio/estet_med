b86b6979b0d1a2ca43b3d31f0ea277494223470f
hikirion <anatoliy.matvienkov38@gmail.com>
2022-04-23 11:58:41 +0300

    minor fix

---
+++ database/migrations/2022_04_22_133033_create_teacher_list.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('surname');
+            $table->string('specialize');
+            $table->string('description');
+            $table->string('photo');
+            $table->string('visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
--- resources/views/layouts/footer_front.blade.php
+++ resources/views/layouts/footer_front.blade.php
@@ -4,38 +4,47 @@
         <h2 class="text-uppercase text-white text-center wow fadeInUp" data-wow-delay=".2s">contact us</h2>
         <div class="row justify-content-center">
             <div class="col-lg-6">
-                <form class="rd-form rd-mailform rd-mailform-custom" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
+                <form class="rd-form rd-mailform rd-mailform-custom" data-form-output="form-output-global"
+                      data-form-type="contact" method="post" action="bat/rd-mailform.php">
                     <div class="row row-lg-30">
                         <div class="col-lg-12">
                             <div class="form-wrap">
-                                <input class="form-input form-input-2" id="contact-name-3" type="text" name="name" data-constraints="@Required">
-                                <label class="form-label form-label-2" for="contact-name-3">Name&nbsp;<span class="text-fourth">*</span></label>
+                                <input class="form-input form-input-2" id="contact-name-3" type="text" name="name"
+                                       data-constraints="@Required">
+                                <label class="form-label form-label-2" for="contact-name-3">Name&nbsp;<span
+                                        class="text-fourth">*</span></label>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <div class="form-wrap">
-                                <input class="form-input form-input-2" id="contact-email-3" type="email" name="email" data-constraints="@Email @Required">
-                                <label class="form-label form-label-2" for="contact-email-3">E-mail&nbsp;<span class="text-fourth">*</span></label>
+                                <input class="form-input form-input-2" id="contact-email-3" type="email" name="email"
+                                       data-constraints="@Email @Required">
+                                <label class="form-label form-label-2" for="contact-email-3">E-mail&nbsp;<span
+                                        class="text-fourth">*</span></label>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <div class="form-wrap">
                                 <label class="form-label form-label-2" for="contact-message-3">Message</label>
-                                <textarea class="form-input form-input-2" id="contact-message-3" name="message" data-constraints="@Required"></textarea>
+                                <textarea class="form-input form-input-2" id="contact-message-3" name="message"
+                                          data-constraints="@Required"></textarea>
                             </div>
                         </div>
                         <div class="col-lg-12 text-center offset-top-30">
-                            <button class="button-2 wow fadeInLeft button button-lg button-icon button-icon-left button-secondary button-ujarak" data-wow-delay=".2s" type="submit"><span class="icon mdi mdi-plus"></span>Send Message</button>
+                            <button
+                                class="button-2 wow fadeInLeft button button-lg button-icon button-icon-left button-secondary button-ujarak"
+                                data-wow-delay=".2s" type="submit"><span class="icon mdi mdi-plus"></span>Send Message
+                            </button>
                         </div>
                     </div>
                 </form>
                 <ul class="list-inline list-inline-sm footer-social-list">
-                    <li><a class="icon fa fa-linkedin" href="#"></a></li>
-                    <li><a class="icon fa fa-twitter" href="#"></a></li>
+                    <li><a class="icon fa fa-youtube" href="#"></a></li>
                     <li><a class="icon fa fa-facebook" href="#"></a></li>
                     <li><a class="icon fa fa-instagram" href="#"></a></li>
                 </ul>
-                <p class="rights"><span>&copy;&nbsp; </span><span class="copyright-year"></span><span>&nbsp;</span><span>Sky Events</span><span>.&nbsp;</span><span>All rights reserved</span><span>.&nbsp;</span><a href="#">Terms and Conditions</a><span>.&nbsp;</span><a href="privacy-policy.html">Privacy Policy</a><span>.</span><span> Design&nbsp;by&nbsp;<a href="https://zemez.io/">Zemez</a></span></p>
+                <p class="rights"><span>&copy;&nbsp; </span><span
+                        class="copyright-year"></span><span>&nbsp;</span><span>Estetmed</span><span>.&nbsp;</span><span>All rights reserved</span>
             </div>
         </div>
     </div>
